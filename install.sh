00def7668cecf05732b0f1c918437e7dd10e8ab6
cryptoprof <cryptoprof@gmail.com>
2020-01-03 01:21:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+currentver="$(lsb_release -r -s)"
+requiredver="18.00"
+#check Ubuntu version
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
+        echo "Greater than or equal to 18.04. Do nothing"
+else
+        #if vesion less than 18.04 add aditional repository for php7.2 . In oldest version of Ubuntu php7.0 by default
+        echo "Less than 18.04"
+        sudo apt-get install software-properties-common python-software-properties
+        sudo add-apt-repository -y ppa:ondrej/php
+        sudo apt-get update
+fi
+
 sudo apt install php7.2 php7.2-curl php7.2-common php7.2-cli php7.2-mysql php7.2-mbstring php7.2-fpm php7.2-xml php7.2-zip -y
 sudo apt install nginx -y
 sudo apt install unzip curl -y
