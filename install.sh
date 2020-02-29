9d9354edfd8c93a990a20f7abebdc4bba7c4d695
cryptoprof <cryptoprof@gmail.com>
2020-02-29 20:00:04 +0300

    Update install.sh
    bugfix

--- install.sh
+++ install.sh
@@ -18,14 +18,13 @@ sudo apt install nginx -y
 sudo apt install unzip curl -y
 sudo apt install mariadb-server -y
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
+sudo cp ./laravel.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/laravel.conf /etc/nginx/sites-enabled/
 sudo mysql_secure_installation
 
 cd /var/www/
 #change to your git repository if doesn't need default laravel project
 sudo composer create-project --prefer-dist laravel/laravel laravel
-sudo cp ./laravel.conf /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/laravel.conf /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
